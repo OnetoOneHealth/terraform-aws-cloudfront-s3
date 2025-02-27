098ffb86bacce92e0acf89ded9e09d5c721937ce
Lang Martin <lang.martin@121.health>
2025-02-27 10:45:33 -0500

    feat: associate a cloudfront function passed in (#2)
    It's a required argument for now, would be more reusable if the inclusion was conditional

--- cloudfront.tf
+++ cloudfront.tf
@@ -40,6 +40,11 @@ resource "aws_cloudfront_distribution" "web" {
       }
     }
 
+    function_association {
+      event_type   = "viewer-response"
+      function_arn = var.default_cache_viewer_response_fun_arn
+    }
+
     viewer_protocol_policy = "redirect-to-https"
 
     min_ttl     = var.min_ttl
